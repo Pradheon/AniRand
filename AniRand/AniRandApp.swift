ffafdb66e39db5dc9e5c6bbfc0cca2574132b7a1
Pradheon <pradheon@gmail.com>
2022-02-09 20:09:22 -0800

    Initial Commit

---
+++ AniRand/AniRandApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniRandApp.swift
+//  AniRand
+//
+//  Created by Joshan Rai on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AniRandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
